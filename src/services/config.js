51b7b352e005d197030fb958a48f3585690c6222
dery <derynusaputra@gmail.com>
2025-10-18 18:05:31 +0700

    fix noh

--- src/services/config.js
+++ src/services/config.js
@@ -1,4 +1,4 @@
 export const configEnv = {
-  // baseUrl: process.env.NEXT_PUBLIC_BASE_URL,
-  baseUrl: "http://localhost:3000",
+  baseUrl: process.env.NEXT_PUBLIC_BASE_URL,
+  // baseUrl: "http://localhost:3000",
 };
